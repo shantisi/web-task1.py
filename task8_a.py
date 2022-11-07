aad73b691b9912e9c6a9547f90ea0657915789ac
shantisi <110821342+shantisi@users.noreply.github.com>
2022-11-07 17:02:00 +0530

    Add files via upload

---
+++ task8_a.py
@@ -0,0 +1,120 @@
+import requests
+from task1 import top_movie
+# from Task4 import scrap
+import json
+import os
+from bs4 import BeautifulSoup
+
+# movie_rank=input('Enter the movie rank no. whose details you want: ')
+# url=top_movie[movie_rank]["link"]
+
+url=top_movie[1]["link"]
+
+
+def movie_details(link):
+    print(link)
+    movie_id=link[-9:-1]
+    print(movie_id)
+    
+
+    filename=movie_id+'.json'
+    text=os.path.exists(filename)
+    if text==True:
+        with open(filename,"r")as f:
+            a=json.load(f)    
+        print(a)   
+    else:
+        # # data=scrap(url) 
+        # with open(filename,"w") as f:
+        #     json.dump(data,f,indent=6)
+        # print(data)  
+        
+
+        def scrap_list():
+            dic={}
+            k=[]
+            url=link
+            print(link)
+            page=requests.get(url)
+            page
+            soup=BeautifulSoup(page.text,"html.parser")
+            movie_name=soup.find("h1",class_="sc-b73cd867-0 eKrKux").get_text()
+            dic["movie_name"]=movie_name
+            director=soup.find("a",class_="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link").get_text()
+            dic["director"]=director
+            poster_image=soup.find("div",class_="ipc-media ipc-media--poster-27x40 ipc-image-media-ratio--poster-27x40 ipc-media--baseAlt ipc-media--poster-l ipc-poster__poster-image ipc-media__img").img['src']
+            dic["postername"]=poster_image
+            runtime=soup.find("li",class_="ipc-metadata-list__item").get_text()
+            print(runtime)
+            # u=runtime[-6:]
+            # dic["Runtime"]=u
+            bio=soup.find("span",class_="sc-16ede01-2 gXUyNh").get_text()
+            dic["bio"]=[bio]
+            abc=soup.find('div',class_="ipc-chip-list__scroller")
+            genre=abc.find_all('span')
+            list1=[]
+            for i in genre:
+                list1.append(i.get_text())
+            dic['genre']=list1
+            Detail=soup.find("section",cel_widget_id="StaticFeature_Details")
+            s=Detail.find_all("div")
+            cast=soup.find("section",)
+            y=[]
+            for ul in s:
+                h=ul.find_all("ul")
+                for li in h:
+                    o=li.find_all("li")
+                    for aa in o:
+                        z=aa.find_all('a')
+                        d=[]
+                        for i in z:
+                            k=i.get_text()
+                            d.append(k)
+                        for i in d:
+                            if i not in y:
+                                y.append(i)
+            list1=["Bangla","Bengali","Bodo", "Dogri", "Gujarati", "Hindi", "Kashmiri", "Kannada", "Konkani", "Maithili", "Malayalam", "Manipuri", "Marathi", "Nepali", "Oriya", "Punjabi", "Tamil", "Telugu", "Santali", "Sindhi", "Urdu","Assamese" ]
+            lang=[]
+            for i in y:
+                for j in list1:
+                    if i==j:
+                        lang.append(i)
+            dic["language"]=lang
+            b=y[1].split()
+            # print(b)
+            dic["release_year"]=b[2]
+            dic["country"]=b[3][1:-1]
+            dic["release_date"]=b[1]+b[0]
+            data=dic
+            with open("filename","w")as f:
+                k=json.dump(dic,f,indent=4)
+
+        scrap_list()
+
+movie_details(url)   
+
+# from task1 import top_movie
+# from task1 import scrap
+# import json
+# import os
+
+# url=top_movie[1]["link"]
+# def movie_details(movie_detail):
+#     movie_id=""
+#     for id in movie_detail[27:-1]:
+#         movie_id+=id
+#         print(movie_id)
+#     filename=movie_id+'.json'
+#     text=os.path.exists(filename)
+#     if text==True:
+#         with open(filename,"r")as f:
+#             a=json.load(f)    
+#         print(a)   
+#     else:
+#         data=scrap(url) 
+#         with open(filename,"w") as f: 
+#             json.dump(data,f,indent=6)
+#         print(data)  
+          
+# movie_details(url)      
+
